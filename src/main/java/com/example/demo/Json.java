0b821b87eb67976baef9a4b9e3c22632791340b0
unknown <vadrevuhemapriya@gmail.com>
2019-06-24 15:09:52 +0530

    CSVcommit

---
+++ src/main/java/com/example/demo/Json.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+import java.io.File;
+
+import java.util.List;
+
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+
+
+
+
+public class Json {
+	
+	  public static void main(String[] args) throws Exception {
+	        File input = new File("src\\\\main\\\\resources\\\\orderLines.csv");
+	        File output = new File("src\\\\main\\\\resources\\\\orderLines.txt");
+	 
+	        CsvSchema csvSchema = CsvSchema.builder().setUseHeader(true).build();
+	        CsvMapper csvMapper = new CsvMapper();
+	 
+	        // Read data from CSV file
+	        List<Object> readAll = csvMapper.readerFor(Map.class).with(csvSchema).readValues(input).readAll();
+	 
+	        ObjectMapper mapper = new ObjectMapper();
+	 
+	        // Write JSON formated data to output.json file
+	        mapper.writerWithDefaultPrettyPrinter().writeValue(output, readAll);
+	 
+	        // Write JSON formated data to stdout
+	        System.out.println(mapper.writerWithDefaultPrettyPrinter().writeValueAsString(readAll));
+	    }
+
+
+
+}
